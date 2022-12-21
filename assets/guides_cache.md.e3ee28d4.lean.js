a42a7b3f82216e530878eadea8e749ef93f89d4e
tutkli <tutkli@users.noreply.github.com>
2022-12-21 13:32:38 +0000

    deploy: 33d91cc7922a81d928d7a3ccd8d28fc17e793e20

---
+++ assets/guides_cache.md.e3ee28d4.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as e}from"./app.8a5d3fe5.js";const d=JSON.parse('{"title":"Cache Configuration","description":"","frontmatter":{},"headers":[],"relativePath":"guides/cache.md"}'),o={name:"guides/cache.md"},p=e("",6),l=[p];function t(c,r,i,C,D,A){return n(),a("div",null,l)}const F=s(o,[["render",t]]);export{d as __pageData,F as default};
