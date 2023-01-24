4f60b59ce51272fcbade50e4e56b656fd4057ca2
tutkli <tutkli@users.noreply.github.com>
2023-01-24 10:37:17 +0000

    deploy: 07288b14304d2cb6a089387b97207d36f961ef84

---
+++ assets/guides_cache.md.a7e7d48d.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as e}from"./app.bce2561f.js";const d=JSON.parse('{"title":"Cache Configuration","description":"","frontmatter":{},"headers":[],"relativePath":"guides/cache.md"}'),o={name:"guides/cache.md"},p=e("",6),l=[p];function t(c,r,i,C,D,A){return n(),a("div",null,l)}const F=s(o,[["render",t]]);export{d as __pageData,F as default};
--- assets/guides_cache.md.b7f8578e.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as n,a as e}from"./app.fba5d4fc.js";const d=JSON.parse('{"title":"Cache Configuration","description":"","frontmatter":{},"headers":[],"relativePath":"guides/cache.md"}'),o={name:"guides/cache.md"},p=e("",6),l=[p];function t(c,r,i,C,D,A){return n(),a("div",null,l)}const F=s(o,[["render",t]]);export{d as __pageData,F as default};
